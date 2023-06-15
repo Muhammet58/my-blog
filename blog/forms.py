9e545f55375f65efa7e38117aeef96a9404e0ffa
muhammet <yildirimuhammed058@gamil.com>
2023-06-15 16:06:50 +0300

    düzenleme

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,7 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'text', 'author']
+        fields = ['title', 'text']
 
         widgets = {
             'title': forms.TextInput(),
--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,7 @@ def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
+            post = form.save(commit=True)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
